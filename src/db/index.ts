35c2fbb5be2972d9862a65f0a9574d8f35b6bbdc
atefzed <atefzed@gmail.com>
2025-02-18 20:45:03 +0100

    add github action

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config'
-// import { drizzle } from 'drizzle-orm/libsql'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 
-const db = drizzle(process.env.DB_FILE_NAME!)
+const db = drizzle(process.env.DB_FILE_NAME! || "./src/db/data.db")
 
 export default db
