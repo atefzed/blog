319bd08ff22b22ce77290a7cbee62392b9cfdeda
atefzed <atefzed@gmail.com>
2025-02-18 20:29:02 +0100

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,15 +17,13 @@ export const metadata: Metadata = {
   description: "A Web Developer based in Paris, FRANCE.",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
-  params: {locale}
 }: Readonly<{
   children: React.ReactNode;
-  params: {locale: string}
 }>) {
   return (
-    <html lang={locale}>
+    <html lang="en">
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
